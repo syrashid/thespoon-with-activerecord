1c8b620515d28d22ce92a2b7cbb63ca6a1731614
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-23 03:07:50 -0500

    FEAT: Coding create and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  # See all my restaurants
-  get 'restaurants', to: 'restaurants#index'
-  # See one of my restaurants
   # Create a restaurant
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+  # Read all my restaurants
+  get 'restaurants', to: 'restaurants#index'
+  # Read one of my restaurants
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
   # Update a restaurant
+  get 'restaurants/:id/edit', to: 'restaurants#edit'
+  patch 'restaurants/:id', to: 'restaurants#update'
+
   # Destroy a restaurant
+  delete 'restaurants/:id', to: 'restaurants#destroy'
 end
